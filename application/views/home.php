bb925e4cf7269a41b838d25464c764fa9047c168
furkandogan99 <frkndgn.99@hotmail.com>
2018-02-14 15:11:27 +0100

    Controllers en views gewijzigd.

---
+++ application/views/home.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Todo</title>
+</head>
+<body>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+				<div class="navbar-inner">
+					<div class="container-fluid">
+						<a class="brand" href="#">myTodo</a>
+						<div class="nav-collapse collapse">
+							<p class="navbar-text pull-right">
+
+
+							</p>
+							<ul class="nav">
+								<li><a href=#">Home</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+	   </div>
+
+	   <div class="container-fluid">
+	   	 <div class="row-fluid">
+	   	 	 <div class="span3">
+	   	 	 	 <div class="well sidebar-nav">
+	   	 	 	 <div style="margin:0 0 10px 10px;">
+	   	 	 	 	
+
+	   	 	 	 </div>
+	   	 	 	 </div>
+	   	 	 </div>
+
+	   	 	 <div class="span9">
+	   	 	 	
+	   	 	 	<h1>Welkom bij mijn Todolist!</h1>
+	   	 	 	<p>Gewoon een simpele todolist.</p>
+	   	 	 </div>
+	   	   </div>
+	   	 <hr>
+
+	   	 <footer>
+	   	   <p>&copy; Furkan Dogan</p>
+	   	 </footer>
+	   </div>
+</body>
+</html>
